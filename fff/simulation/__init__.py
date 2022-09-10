a265d538563ccbe9b0cdeb08a1921e51ac84e50b
Logan Ward <ward.logan.t@gmail.com>
2022-09-10 08:58:58 -0400

    Get around Psi4 not being serializable

--- fff/simulation/__init__.py
+++ fff/simulation/__init__.py
@@ -5,23 +5,32 @@ import os
 
 from ase.calculators.calculator import Calculator
 from ase import Atoms
+from ase.calculators.psi4 import Psi4
 
 
-def run_calculator(atoms: Atoms, calc: Calculator, temp_path: Optional[str] = None) -> Atoms:
+def run_calculator(atoms: Atoms, calc: Calculator | dict, temp_path: Optional[str] = None) -> Atoms:
     """Run an NWChem computation on the requested cluster
 
     Args:
         atoms: Cluster to evaluate
-        calc: ASE calculator to use
+        calc: ASE calculator to use. Either the calculator object or a dictionary describing the settings
+            (only Psi4 supported at the moment for dict)
         temp_path: Base path for the scratch files
     Returns:
         Atoms after the calculation
     """
 
     atoms = atoms.copy()  # Make a duplicate
+
     with TemporaryDirectory(dir=temp_path, prefix='fff') as temp_dir:
         # Execute from the temp so that the calculators do not interfere
         os.chdir(temp_dir)
+
+        # Special case for Psi4 which sets the run directory on creating the object
+        if isinstance(calc, dict):
+            calc = calc.copy()
+            assert calc.pop('calc') == 'psi4', 'only psi4 is supported for now'
+            calc = Psi4(**calc, directory=temp_dir)
 
         # Run the calculation
         atoms.calc = calc
--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -1,42 +1,41 @@
 """Test features related to simulation"""
+import pickle as pkl
 
 from ase.calculators.lj import LennardJones
+from ase.calculators.psi4 import Psi4
 from ase.calculators.singlepoint import SinglePointCalculator
-from ase.build import bulk
+from ase.build import molecule
 from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
-
-from pytest import fixture
-
+from pytest import fixture, mark
 
 from fff.simulation import run_calculator
 from fff.simulation.md import run_dynamics
 
 
 @fixture()
-def calc():
-    return LennardJones()
-
-
-@fixture()
 def atoms():
-    return bulk('Cu') * [4, 4, 4]
+    return molecule('H2O')
 
 
+@mark.parametrize(
+    'calc', [LennardJones(), {'calc': 'psi4', 'method': 'hf', 'basis': 'sto-3g'}]
+)
 def test_single(calc, atoms):
     atoms = run_calculator(atoms, calc)
-    assert len(atoms) == 64
+    assert len(atoms) == 3
     assert 'forces' in atoms.calc.results  # Ensure that forces have been computed
     assert 'energy' in atoms.calc.results
 
 
-def test_md(calc, atoms):
+def test_md(atoms):
+    calc = LennardJones()
     MaxwellBoltzmannDistribution(atoms, temperature_K=60)
     traj = run_dynamics(atoms, calc, 1, 1000, log_interval=100)
     assert len(traj) == 11
 
     # Make sure it has both the energy and the forces
     assert isinstance(traj[0].calc, SinglePointCalculator)  # SPC is used to store results
-    assert traj[0].get_forces().shape == (64, 3)
+    assert traj[0].get_forces().shape == (3, 3)
     assert traj[0].get_total_energy()
     assert traj[0].get_total_energy() != traj[-1].get_total_energy()
 
