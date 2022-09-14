93fa2ae36849e47f551b4a5ae7cb616af0f08dfb
Logan Ward <ward.logan.t@gmail.com>
2022-09-14 18:21:24 +0000

    Figure for the coordination policy

---
+++ colmena/initial/theta-test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Test for the local system
+python run.py \
+    --ml-endpoint ae6479e9-61e6-4582-a34a-c17af560e535 \
+    --qc-endpoint 698fba9a-4b12-4e0b-b83a-be6ded509946 \
+    --training-set ../../notebooks/initial-database/initial-psi4.db \
+    --search-space ../../notebooks/initial-database/initial-psi4.db \
+    --starting-model ../../notebooks/psi4-integration/starting-psi4-model \
+    --num-qc-workers 8 \
+    --run-length 50 \
+    --num-epochs 128 \
+    --ensemble-size 4 \
+    --infer-chunk-size 1000 \
+    --infer-pool-size 4 \
+    --retrain-freq 100 \
+    --num-to-run 1000
