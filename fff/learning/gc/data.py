0d8a67e610aceb43b7d4632b700045b8b711e56c
Logan Ward <ward.logan.t@gmail.com>
2022-11-09 16:54:24 -0500

    Refactored to split off conversion function

--- fff/learning/gc/data.py
+++ fff/learning/gc/data.py
@@ -9,7 +9,46 @@ import torch
 from torch.nn import functional as F
 from torch_geometric.data import InMemoryDataset, Data
 
+default_types = {'H': 0, 'O': 1}
 
+
+def convert_atoms_to_pyg(mol: Atoms, types: Optional[dict[str, int]] = None) -> Data:
+    """Convert an Atoms object to a PyG Data object
+
+    Args:
+        mol: Atoms object to be converted
+        types: Mapping of element to type in the network. Default is for O->0, H->1
+    Returns:
+        Converted object ready to be used in a PyG SchNet model
+    """
+
+    # Use default types if non provided
+    if types is None:
+        types = default_types
+
+    # Only operate if there is a calculator
+    y = None  # potential energy
+    f = None  # gradients (forces)
+    if mol.calc is not None:
+        results = mol.calc.results
+        if 'energy' in results:
+            y = torch.tensor(mol.get_potential_energy(), dtype=torch.float)  # potential energy
+        if 'forces' in results:
+            f = torch.tensor(mol.get_forces(), dtype=torch.float)  # gradients
+
+    pos = mol.get_positions() - mol.get_center_of_mass()
+    pos = torch.tensor(pos, dtype=torch.float)  # coordinates
+
+    # Convert it to a PyG data record
+    size = int(pos.size(dim=0) / 3)
+    type_idx = [types.get(i) for i in mol.get_chemical_symbols()]
+    atomic_number = mol.get_atomic_numbers()
+    z = torch.tensor(atomic_number, dtype=torch.long)
+    x = F.one_hot(torch.tensor(type_idx, dtype=torch.long), num_classes=len(types))
+    return Data(x=x, z=z, pos=pos, y=y, f=f, n_atoms=len(mol), size=size)
+
+
+# TODO (wardlt): Write diskless version
 class AtomsDataset(InMemoryDataset):
     """Dataset created from a list of """
 
@@ -47,7 +86,6 @@ class AtomsDataset(InMemoryDataset):
 
     def process(self):
         # NB: coding for atom types
-        types = {'H': 0, 'O': 1}
         data_list = []
 
         # Loop over all datasets (there should be only one)
@@ -58,28 +96,7 @@ class AtomsDataset(InMemoryDataset):
                     # Extract the data from the ASE records
                     name = ['energy', 'forces']
                     mol: Atoms = row.toatoms()
-
-                    # Only operate if there is a calculator
-                    y = None  # potential energy
-                    f = None  # gradients (forces)
-                    if mol.calc is not None:
-                        results = mol.calc.results
-                        if 'energy' in results:
-                            y = torch.tensor(mol.get_potential_energy(), dtype=torch.float)  # potential energy
-                        if 'forces' in results:
-                            f = torch.tensor(mol.get_forces(), dtype=torch.float)  # gradients)
-
-                    pos = mol.get_positions() - mol.get_center_of_mass()
-                    pos = torch.tensor(pos, dtype=torch.float)  # coordinates
-
-                    # Convert it to a PyG data record
-                    size = int(pos.size(dim=0) / 3)
-                    type_idx = [types.get(i) for i in mol.get_chemical_symbols()]
-                    atomic_number = mol.get_atomic_numbers()
-                    z = torch.tensor(atomic_number, dtype=torch.long)
-                    x = F.one_hot(torch.tensor(type_idx, dtype=torch.long), num_classes=len(types))
-
-                    data = Data(x=x, z=z, pos=pos, y=y, f=f, n_atoms=len(mol), size=size, name=name)
+                    data = convert_atoms_to_pyg(mol)
 
                     if self.pre_filter is not None and not self.pre_filter(data):
                         continue
